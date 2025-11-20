def81087fd5aec487d2965618cd1f45110fbc78a
Kåre Birger Lapstuen <lapstue@gmail.com>
2025-11-20 22:10:03 +0100

    Rip out unused db setup

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,11 @@ mod handlers;
 mod models;
 mod routers;
 use axum::Router;
-use sqlx::sqlite::SqlitePoolOptions;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let db = SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect("sqlite://prod.db")
-        .await?;
-
     let app = Router::new()
         .nest("/suppliers", routers::get_supplier_router())
         .nest("/transactions", routers::get_transaction_router());
